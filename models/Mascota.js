bf12cfd3fd245b48c7570fc5dee7290c8332ca73
MartinLupa <lupa_martin@hotmail.com>
2022-03-24 20:17:35 +0100

    Mascota schema created

---
+++ models/Mascota.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const mascotaSchema = new mongoose.Schema(
+  {
+    nombre: { type: String, required: true },
+    especie: { type: String, required: true },
+    genero: { type: String, required: true },
+    edad: { type: String, required: true },
+    fecha_nacimiento: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("mascota", mascotaSchema);
